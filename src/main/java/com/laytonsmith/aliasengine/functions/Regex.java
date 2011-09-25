92499260eff2c9e2f55e59c1431855bb6cb23ec1
Layton Smith <layton@laytonsmith.com>
2011-09-25 17:03:38 -0500

    Regex functions are finished.

--- src/main/java/com/laytonsmith/aliasengine/functions/Regex.java
+++ src/main/java/com/laytonsmith/aliasengine/functions/Regex.java
@@ -5,10 +5,13 @@
 package com.laytonsmith.aliasengine.functions;
 
 import com.laytonsmith.aliasengine.Constructs.CArray;
+import com.laytonsmith.aliasengine.Constructs.CInt;
+import com.laytonsmith.aliasengine.Constructs.CString;
 import com.laytonsmith.aliasengine.Constructs.Construct;
 import com.laytonsmith.aliasengine.functions.Exceptions.ExceptionType;
 import com.laytonsmith.aliasengine.functions.exceptions.ConfigRuntimeException;
 import java.io.File;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.bukkit.command.CommandSender;
 
@@ -63,7 +66,74 @@ public class Regex {
         }
 
         public Construct exec(int line_num, File f, CommandSender p, Construct... args) throws ConfigRuntimeException {
-            throw new UnsupportedOperationException("Not supported yet.");
+            Pattern pattern = getPattern(args[0], line_num, f);
+            String subject = args[1].val();
+            CArray ret = new CArray(line_num, f);
+            Matcher m = pattern.matcher(subject);
+            if(m.find()){
+                ret.push(new CString(m.group(0), line_num, f));
+
+                for(int i = 1; i <= m.groupCount(); i++){
+                    ret.push(new CString(m.group(i), line_num, f));
+                }
+            }
+            return ret;
+        }
+        
+    }
+    
+    @api public static class reg_match_all implements Function{
+
+        public String getName() {
+            return "reg_match_all";
+        }
+
+        public Integer[] numArgs() {
+            return new Integer[]{2};
+        }
+
+        public String docs() {
+            return "array {pattern, subject} Searches subject for all matches to the regular expression given in pattern, unlike reg_match,"
+                    + " which just returns the first match.";
+        }
+
+        public ExceptionType[] thrown() {
+            return new ExceptionType[]{};
+        }
+
+        public boolean isRestricted() {
+            return false;
+        }
+
+        public void varList(IVariableList varList) {}
+
+        public boolean preResolveVariables() {
+            return true;
+        }
+
+        public String since() {
+            return "3.2.0";
+        }
+
+        public Boolean runAsync() {
+            return null;
+        }
+
+        public Construct exec(int line_num, File f, CommandSender p, Construct... args) throws ConfigRuntimeException {
+            Pattern pattern = getPattern(args[0], line_num, f);
+            String subject = args[1].val();
+            CArray fret = new CArray(line_num, f);
+            Matcher m = pattern.matcher(subject);
+            while(m.find()){
+                CArray ret = new CArray(line_num, f);
+                ret.push(new CString(m.group(0), line_num, f));
+
+                for(int i = 1; i <= m.groupCount(); i++){
+                    ret.push(new CString(m.group(i), line_num, f));
+                }
+                fret.push(ret);
+            }
+            return fret;
         }
         
     }
@@ -106,7 +176,14 @@ public class Regex {
         }
 
         public Construct exec(int line_num, File f, CommandSender p, Construct... args) throws ConfigRuntimeException {
-            throw new UnsupportedOperationException("Not supported yet.");
+            Pattern pattern = getPattern(args[0], line_num, f);
+            String replacement = args[1].val();
+            String subject = args[2].val();
+            String ret = "";
+            
+            ret = pattern.matcher(subject).replaceAll(replacement);
+            
+            return new CString(ret, line_num, f);
         }
         
     }
@@ -122,7 +199,8 @@ public class Regex {
         }
 
         public String docs() {
-            return "array {pattern, subject}";
+            return "array {pattern, subject} Splits a string on the given regex, and returns an array of the parts. If"
+                    + " nothing matched, an array with one element, namely the original subject, is returned.";
         }
 
         public ExceptionType[] thrown() {
@@ -148,8 +226,16 @@ public class Regex {
         }
 
         public Construct exec(int line_num, File f, CommandSender p, Construct... args) throws ConfigRuntimeException {
-            throw new UnsupportedOperationException("Not supported yet.");
+            Pattern pattern = getPattern(args[0], line_num, f);
+            String subject = args[1].val();
+            String [] rsplit = pattern.split(subject);
+            CArray ret = new CArray(line_num, f);
+            for(String split : rsplit){
+                ret.push(new CString(split, line_num, f));
+            }
+            return ret;
         }
+        
         
     }  
     
@@ -164,7 +250,7 @@ public class Regex {
         }
 
         public String docs() {
-            return "int {pattern, subject}";
+            return "int {pattern, subject} Counts the number of occurances in the subject.";
         }
 
         public ExceptionType[] thrown() {
@@ -190,7 +276,14 @@ public class Regex {
         }
 
         public Construct exec(int line_num, File f, CommandSender p, Construct... args) throws ConfigRuntimeException {
-            throw new UnsupportedOperationException("Not supported yet.");
+            Pattern pattern = getPattern(args[0], line_num, f);
+            String subject = args[1].val();
+            long ret = 0;
+            Matcher m = pattern.matcher(subject);
+            while(m.find()){
+                ret++;
+            }
+            return new CInt(ret, line_num, f);
         }
         
     }
--- src/test/java/com/laytonsmith/aliasengine/functions/RegexTest.java
+++ src/test/java/com/laytonsmith/aliasengine/functions/RegexTest.java
@@ -5,6 +5,7 @@
 package com.laytonsmith.aliasengine.functions;
 
 import com.laytonsmith.aliasengine.functions.exceptions.ConfigCompileException;
+import com.laytonsmith.aliasengine.functions.exceptions.ConfigRuntimeException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -39,24 +40,36 @@ public class RegexTest {
     public void tearDown() {
     }
 
-//    @Test public void testRegMatch() throws ConfigCompileException{
-//        assertEquals("{word}", SRun("reg_match('word', 'This is a word')", null));
-//        assertEquals("{}", SRun("reg_match('word', 'This is an airplane')", null));
-//        assertEquals("{word, word}", SRun("reg_match('word', 'This is a word')", null));
-//        assertEquals("{This is a word, word}", SRun("reg_match('This is a (word)', 'This is a word')", null));
-//    }
-//    
-//    @Test public void testRegReplace() throws ConfigCompileException{
-//        assertEquals("word", SRun("reg_replace('This is a (word)', '$1', 'This is a word')", null));
-//        assertEquals("It's a wordy day!", SRun("reg_replace('sunn', 'word', 'It\\'s a sunny day!')", null));
-//    }
-//    
-//    @Test public void testRegSplit() throws ConfigCompileException{
-//        assertEquals("{one, two, three}", SRun("reg_split('|', 'one|two|three')", null));
-//    }
-//    
-//    @Test public void testRegCount() throws ConfigCompileException{
-//        assertEquals("3", SRun("reg_count('/', '///yay')", null));
-//        assertEquals("0", SRun("reg_count('poppycock', 'tiddly winks')", null));
-//    }
+    @Test public void testRegMatch() throws ConfigCompileException{
+        assertEquals("{word}", SRun("reg_match('word', 'This is a word')", null));
+        assertEquals("{}", SRun("reg_match('word', 'This is an airplane')", null));
+        assertEquals("{word, word}", SRun("reg_match('(word)', 'This is a word')", null));
+        assertEquals("{This is a word, word}", SRun("reg_match('This is a (word)', 'This is a word')", null));
+        assertEquals("{WORD}", SRun("reg_match(array(word, i), 'THIS IS A WORD')", null));
+        try{
+            SRun("reg_match(array(word, l), hi)", null);
+            fail();
+        } catch(ConfigRuntimeException e){
+            //Pass
+        }
+    }
+    
+    @Test public void testRegMatchAll() throws ConfigCompileException{
+        assertEquals("{{This is a word, word}, {This is a word, word}}", SRun("reg_match_all('This is a (word)', 'word, This is a word, This is a word')", null));
+        assertEquals("{}", SRun("reg_match_all('word', 'yay')", null));
+    }
+    
+    @Test public void testRegReplace() throws ConfigCompileException{
+        assertEquals("word", SRun("reg_replace('This is a (word)', '$1', 'This is a word')", null));
+        assertEquals("It's a wordy day!", SRun("reg_replace('sunn', 'word', 'It\\'s a sunny day!')", null));
+    }
+    
+    @Test public void testRegSplit() throws ConfigCompileException{
+        assertEquals("{one, two, three}", SRun("reg_split('\\\\|', 'one|two|three')", null));
+    }
+    
+    @Test public void testRegCount() throws ConfigCompileException{
+        assertEquals("3", SRun("reg_count('/', '///yay')", null));
+        assertEquals("0", SRun("reg_count('poppycock', 'tiddly winks')", null));
+    }
 }
