9b945f2a1867560739b6a5c5a9cd1d0d10cab18a
Layton Smith <laytonsmith14@gmail.com>
2012-07-16 16:40:16 -0500

    Stupid bash.

--- src/main/resources/interpreter-helpers/bash.sh
+++ src/main/resources/interpreter-helpers/bash.sh
@@ -5,5 +5,5 @@ if [ "$#" -eq 0 ]; then
 else
         SCRIPT="$1"
         shift 1
-        cat "$SCRIPT" | java -jar "%%LOCATION%%" --interpreter "$SCRIPT" $@
+        cat "$SCRIPT" | java -jar "%%LOCATION%%" --interpreter "$SCRIPT" "$@"
 fi
