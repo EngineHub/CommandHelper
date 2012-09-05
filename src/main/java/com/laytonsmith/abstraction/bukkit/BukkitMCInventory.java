7c4280a2234d57b5cff891d24103a03b43cd46e3
Layton Smith <layton@laytonsmith.com>
2012-09-04 23:51:56 -0500

    Fixed a bug with player inventory not refreshing when using pgive_item and associated functions.

--- src/main/java/com/laytonsmith/abstraction/bukkit/BukkitMCInventory.java
+++ src/main/java/com/laytonsmith/abstraction/bukkit/BukkitMCInventory.java
@@ -5,6 +5,7 @@ package com.laytonsmith.abstraction.bukkit;
 import com.laytonsmith.abstraction.AbstractionObject;
 import com.laytonsmith.abstraction.MCInventory;
 import com.laytonsmith.abstraction.MCItemStack;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.PlayerInventory;
 
 /**
@@ -37,22 +38,37 @@ public class BukkitMCInventory implements MCInventory {
 
     public void setItem(int slot, MCItemStack stack) {
         this.i.setItem(slot, stack==null?null:((BukkitMCItemStack)stack).is);
+		if(this.i.getHolder() instanceof Player){
+			((Player)this.i.getHolder()).updateInventory();
+		}
     }
 
     public void setHelmet(MCItemStack stack) {
         this.i.setHelmet(((BukkitMCItemStack)stack).is);
+		if(this.i.getHolder() instanceof Player){
+			((Player)this.i.getHolder()).updateInventory();
+		}
     }
 
     public void setChestplate(MCItemStack stack) {
         this.i.setChestplate(((BukkitMCItemStack)stack).is);
+		if(this.i.getHolder() instanceof Player){
+			((Player)this.i.getHolder()).updateInventory();
+		}
     }
 
     public void setLeggings(MCItemStack stack) {
         this.i.setLeggings(((BukkitMCItemStack)stack).is);
+		if(this.i.getHolder() instanceof Player){
+			((Player)this.i.getHolder()).updateInventory();
+		}
     }
 
     public void setBoots(MCItemStack stack) {
         this.i.setBoots(((BukkitMCItemStack)stack).is);
+		if(this.i.getHolder() instanceof Player){
+			((Player)this.i.getHolder()).updateInventory();
+		}
     }
 
     public MCItemStack getHelmet() {
