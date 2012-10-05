ad301dbd263394c526eb3ded3136ceaa6942e2ed
Jim Bilbrey <jb_aero@verizon.net>
2012-10-04 22:39:17 -0400

    Add LightningStrike entity to abstraction

---
+++ src/main/java/com/laytonsmith/abstraction/MCLightningStrike.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.laytonsmith.abstraction;
+
+/**
+ *
+ * @author Jim
+ */
+public interface MCLightningStrike extends MCEntity{
+
+	public boolean isEffect();
+}
--- src/main/java/com/laytonsmith/abstraction/bukkit/BukkitConvertor.java
+++ src/main/java/com/laytonsmith/abstraction/bukkit/BukkitConvertor.java
@@ -141,6 +141,10 @@ public class BukkitConvertor implements Convertor {
 			return new BukkitMCItem((Item)be);
 		}
 		
+		if(be instanceof LightningStrike){
+			return new BukkitMCLightningStrike((LightningStrike)be);
+		}
+		
 		if(be instanceof ExperienceOrb){
 			return new BukkitMCExperienceOrb((ExperienceOrb)be);
 		}
