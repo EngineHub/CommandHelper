b84233fc73624cd7fecda978eaad442c47b9311b
Layton Smith <layton@laytonsmith.com>
2013-01-04 18:33:08 -0600

    Merged in from master.

--- src/main/java/com/laytonsmith/core/constructs/Construct.java
+++ src/main/java/com/laytonsmith/core/constructs/Construct.java
@@ -68,14 +68,12 @@ public abstract class Construct implements Cloneable, Comparable<Construct>, Mix
     public Construct(String value, ConstructType ctype, int line_num, File file, int column) {
         this.value = value;
 		Static.AssertNonNull(value, "The string value may not be null.");
-        this.ctype = ctype;
         this.target = new Target(line_num, file, column);
     }
     
     public Construct(String value, ConstructType ctype, Target t){
         this.value = value;
 		Static.AssertNonNull(value, "The string value may not be null.");
-        this.ctype = ctype;
         this.target = t;
     }
 
