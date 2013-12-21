fb356bf24ad9476932941f5fc54cb2d86e269ac1
Jason Unger <EntityReborn@Gmail.com>
2013-12-21 11:31:21 -0800

    Deprecated @startup and @shutdown, to encourage the use of LifeCycle classes. Instead, use `@MSExtension("extname") public class MyClass extends AbstractExtension { }`, which exposes lifecycle functionality.

--- src/main/java/com/laytonsmith/annotations/shutdown.java
+++ src/main/java/com/laytonsmith/annotations/shutdown.java
@@ -11,6 +11,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Deprecated // Use LifeCycle classes instead.
 public @interface shutdown {
 	
 }
