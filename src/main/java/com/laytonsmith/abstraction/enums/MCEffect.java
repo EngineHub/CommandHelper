e3f546679384eeb2f017bee1e33671fb18f419f2
Cailin Smith <savannahcailin@gmail.com>
2014-12-02 10:27:36 -0600

    Updated MCEffect to 1.8

--- src/main/java/com/laytonsmith/abstraction/enums/MCEffect.java
+++ src/main/java/com/laytonsmith/abstraction/enums/MCEffect.java
@@ -34,15 +34,62 @@ public enum MCEffect {
      * Sound of zombies destroying a door.
      */
     ZOMBIE_DESTROY_DOOR(1012),
-    MOBSPAWNER_FLAMES(2004);
+    MOBSPAWNER_FLAMES(2004),
+	FIREWORKS_SPARK("fireworksSpark"),
+	CRIT("crit"),
+	MAGIC_CRIT("magicCrit"),
+	POTION_SWIRL("mobSpell"),
+	POTION_SWIRL_TRANSPARENT("mobSpellAmbient"),
+	SPELL("spell"),
+	INSTANT_SPELL("instantSpell"),
+	WITCH_MAGIC("witchMagic"),
+	NOTE("note"),
+	PORTAL("portal"),
+	FLYING_GLYPH("enchantmenttable"),
+	FLAME("flame"),
+	LAVA_POP("lava"),
+	FOOTSTEP("footstep"),
+	SPLASH("splash"),
+	PARTICLE_SMOKE("smoke"),
+	EXPLOSION_HUGE("hugeexplosion"),
+	EXPLOSION_LARGE("largeexplode"),
+	EXPLOSION("explode"),
+	VOID_FOG("depthsuspend"),
+	SMALL_SMOKE("townaura"),
+	CLOUD("cloud"),
+	COLOURED_DUST("reddust"),
+	SNOWBALL_BREAK("snowballpoof"),
+	WATERDRIP("dripWater"),
+	LAVADRIP("dripLava"),
+	SNOW_SHOVEL("snowshovel"),
+	SLIME("slime"),
+	HEART("heart"),
+	VILLAGER_THUNDERCLOUD("angryVillager"),
+	HAPPY_VILLAGER("happyVillager"),
+	LARGE_SMOKE("largesmoke"),
+	ITEM_BREAK("iconcrack"),
+	TILE_BREAK("blockcrack"),
+	TILE_DUST("blockdust")
+	;
 
     private final int id;
+	private final String particleName;
 
-    MCEffect(int id) {
+    private MCEffect(int id) {
         this.id = id;
+		particleName = null;
     }
+
+	private MCEffect(String particleName){
+		this.id = 0;
+		this.particleName = particleName;
+	}
 
     public int getId() {
         return this.id;
     }
+
+	public String getName(){
+		return particleName;
+	}
 }
