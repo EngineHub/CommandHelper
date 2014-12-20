d78f8a97d157e87cdde98397ecd2037acbcbd8a4
PseudoKnight <michael.jt.smith@gmail.com>
2014-12-19 22:12:17 -0800

    Added minecart display block to entity_spec

--- src/main/java/com/laytonsmith/abstraction/bukkit/entities/BukkitMCMinecart.java
+++ src/main/java/com/laytonsmith/abstraction/bukkit/entities/BukkitMCMinecart.java
@@ -1,8 +1,11 @@
 package com.laytonsmith.abstraction.bukkit.entities;
 
+import com.laytonsmith.abstraction.MCMaterialData;
+import com.laytonsmith.abstraction.bukkit.BukkitMCMaterialData;
 import com.laytonsmith.abstraction.bukkit.BukkitMCVehicle;
 import com.laytonsmith.abstraction.entities.MCMinecart;
 import org.bukkit.entity.Minecart;
+import org.bukkit.material.MaterialData;
 
 public class BukkitMCMinecart extends BukkitMCVehicle
 		implements MCMinecart {
@@ -41,5 +44,25 @@ public class BukkitMCMinecart extends BukkitMCVehicle
 	@Override
 	public void setSlowWhenEmpty(boolean slow) {
 		m.setSlowWhenEmpty(slow);
+	}
+
+	@Override
+	public void setDisplayBlock(MCMaterialData material) {
+		m.setDisplayBlock((MaterialData) material.getHandle());
+	}
+
+	@Override
+	public MCMaterialData getDisplayBlock() {
+		return new BukkitMCMaterialData(m.getDisplayBlock());
+	}
+
+	@Override
+	public void setDisplayBlockOffset(int offset) {
+		m.setDisplayBlockOffset(offset);
+	}
+
+	@Override
+	public int getDisplayBlockOffset() {
+		return m.getDisplayBlockOffset();
 	}
 }
--- src/main/java/com/laytonsmith/abstraction/entities/MCMinecart.java
+++ src/main/java/com/laytonsmith/abstraction/entities/MCMinecart.java
@@ -1,5 +1,6 @@
 package com.laytonsmith.abstraction.entities;
 
+import com.laytonsmith.abstraction.MCMaterialData;
 import com.laytonsmith.abstraction.MCVehicle;
 
 public interface MCMinecart extends MCVehicle {
@@ -9,4 +10,8 @@ public interface MCMinecart extends MCVehicle {
 	public void setMaxSpeed(double speed);
 	public boolean isSlowWhenEmpty();
 	public void setSlowWhenEmpty(boolean slow);
+	public void setDisplayBlock(MCMaterialData material);
+	public MCMaterialData getDisplayBlock();
+	public void setDisplayBlockOffset(int offset);
+	public int getDisplayBlockOffset();
 }
