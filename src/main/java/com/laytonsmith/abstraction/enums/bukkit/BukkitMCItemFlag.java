bf9af35e14c4bf09918f7397aab819c5cd16a281
PseudoKnight <michael.jt.smith@gmail.com>
2015-08-29 22:26:10 -0700

    Address specific enum verification errors on 1.7.x

--- src/main/java/com/laytonsmith/abstraction/enums/bukkit/BukkitMCItemFlag.java
+++ src/main/java/com/laytonsmith/abstraction/enums/bukkit/BukkitMCItemFlag.java
@@ -3,7 +3,9 @@ package com.laytonsmith.abstraction.enums.bukkit;
 import com.laytonsmith.abstraction.Implementation;
 import com.laytonsmith.abstraction.enums.EnumConvertor;
 import com.laytonsmith.abstraction.enums.MCItemFlag;
+import com.laytonsmith.abstraction.enums.MCVersion;
 import com.laytonsmith.annotations.abstractionenum;
+import com.laytonsmith.core.Static;
 import org.bukkit.inventory.ItemFlag;
 
 /**
@@ -21,5 +23,13 @@ public class BukkitMCItemFlag extends EnumConvertor<MCItemFlag, ItemFlag> {
 			instance = new BukkitMCItemFlag();
 		}
 		return instance;
+	}
+
+	@Override
+	protected ItemFlag getConcreteEnumCustom(MCItemFlag abstracted) {
+		if(Static.getServer().getMinecraftVersion().lt(MCVersion.MC1_8)) {
+			return null;
+		}
+		return super.getConcreteEnumCustom(abstracted);
 	}
 }
