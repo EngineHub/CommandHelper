3eaf66ce50e34918d1b425e722d8f1893d9b0293
PseudoKnight <michael.jt.smith@gmail.com>
2016-12-01 03:41:09 -0800

    Do not attempt to register FireworkExplodeEvent prior to 1.8.8

--- src/main/java/com/laytonsmith/core/events/Driver.java
+++ src/main/java/com/laytonsmith/core/events/Driver.java
@@ -47,7 +47,7 @@ public enum Driver {
 	PROJECTILE_LAUNCH,
 	TARGET_ENTITY,
 	ENTITY_TOGGLE_GLIDE(MCVersion.MC1_9),
-	FIREWORK_EXPLODE,
+	FIREWORK_EXPLODE(MCVersion.MC1_8_X),
 
 	/**
 	 * Inventory events
