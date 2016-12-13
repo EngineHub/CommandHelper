fceab99b9aea3e805f6bed9f1c6382fe1f194229
EntryPoint <entrypoint_@naver.com>
2016-12-13 22:50:12 +0900

    Add entity_regain_health event

---
+++ src/main/java/com/laytonsmith/abstraction/enums/MCRegainReason.java
@@ -0,0 +1,16 @@
+package com.laytonsmith.abstraction.enums;
+
+/**
+ * Created by EntryPoint on 2016-12-13.
+ */
+public enum MCRegainReason {
+    REGEN,
+    SATIATED,
+    EATING,
+    ENDER_CRYSTAL,
+    MAGIC,
+    MAGIC_REGEN,
+    WITHER_SPAWN,
+    WITHER,
+    CUSTOM
+}
---
+++ src/main/java/com/laytonsmith/abstraction/events/MCEntityRegainHealthEvent.java
@@ -0,0 +1,14 @@
+package com.laytonsmith.abstraction.events;
+
+import com.laytonsmith.abstraction.enums.MCRegainReason;
+import com.laytonsmith.core.events.BindableEvent;
+
+/**
+ * Created by EntryPoint on 2016-12-13.
+ */
+public interface MCEntityRegainHealthEvent extends BindableEvent {
+    double getAmount();
+    void setAmount(double amount);
+
+    MCRegainReason getRegainReason();
+}
