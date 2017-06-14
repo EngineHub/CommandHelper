e508ad722810579545cd376b99fc7ac0d4e80191
PseudoKnight <michael.jt.smith@gmail.com>
2017-06-14 09:51:29 -0700

    Remove unused Boat deprecations

--- src/main/java/com/laytonsmith/abstraction/bukkit/entities/BukkitMCBoat.java
+++ src/main/java/com/laytonsmith/abstraction/bukkit/entities/BukkitMCBoat.java
@@ -3,15 +3,13 @@ package com.laytonsmith.abstraction.bukkit.entities;
 import com.laytonsmith.abstraction.entities.MCBoat;
 import org.bukkit.entity.Boat;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Vehicle;
 
-public class BukkitMCBoat extends BukkitMCVehicle
-		implements MCBoat {
+public class BukkitMCBoat extends BukkitMCVehicle implements MCBoat {
 
 	Boat b;
 
 	public BukkitMCBoat(Entity e) {
-		super((Vehicle) e);
+		super(e);
 		this.b = (Boat) e;
 	}
 
@@ -23,35 +21,5 @@ public class BukkitMCBoat extends BukkitMCVehicle
 	@Override
 	public void setMaxSpeed(double speed) {
 		b.setMaxSpeed(speed);
-	}
-
-	@Override
-	public double getOccupiedDeclaration() {
-		return b.getOccupiedDeceleration();
-	}
-
-	@Override
-	public void setOccupiedDeclaration(double rate) {
-		b.setOccupiedDeceleration(rate);
-	}
-
-	@Override
-	public double getUnoccupiedDeclaration() {
-		return b.getOccupiedDeceleration();
-	}
-
-	@Override
-	public void setUnoccupiedDeclaration(double rate) {
-		b.setUnoccupiedDeceleration(rate);
-	}
-
-	@Override
-	public boolean getWorkOnLand() {
-		return b.getWorkOnLand();
-	}
-
-	@Override
-	public void setWorkOnLand(boolean workOnLand) {
-		b.setWorkOnLand(workOnLand);
 	}
 }
