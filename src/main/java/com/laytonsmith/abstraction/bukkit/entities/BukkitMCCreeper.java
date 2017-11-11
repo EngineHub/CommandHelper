5dd9e82eb5befc4f978f4740cc70c20f657e0711
PseudoKnight <michael.jt.smith@gmail.com>
2017-11-10 22:28:57 -0800

    Add maxfuseticks and explosionradius to Creeper entity_spec()

--- src/main/java/com/laytonsmith/abstraction/bukkit/entities/BukkitMCCreeper.java
+++ src/main/java/com/laytonsmith/abstraction/bukkit/entities/BukkitMCCreeper.java
@@ -6,10 +6,6 @@ import com.laytonsmith.abstraction.entities.MCCreeper;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 
-/**
- *
- * @author Jason Unger <entityreborn@gmail.com>
- */
 public class BukkitMCCreeper extends BukkitMCLivingEntity implements MCCreeper {
 	Creeper creeper;
 
@@ -30,5 +26,43 @@ public class BukkitMCCreeper extends BukkitMCLivingEntity implements MCCreeper {
 	@Override
 	public void setPowered(boolean powered) {
 		creeper.setPowered(powered);
+	}
+
+	@Override
+	public int getMaxFuseTicks() {
+		try {
+			return creeper.getMaxFuseTicks();
+		} catch(NoSuchMethodError ex) {
+			// Probably prior to 1.12.2
+			return 30;
+		}
+	}
+
+	@Override
+	public void setMaxFuseTicks(int ticks) {
+		try {
+			creeper.setMaxFuseTicks(ticks);
+		} catch(NoSuchMethodError ex){
+			// Probably prior to 1.12.2
+		}
+	}
+
+	@Override
+	public int getExplosionRadius() {
+		try {
+			return creeper.getExplosionRadius();
+		} catch(NoSuchMethodError ex) {
+			// Probably prior to 1.12.2
+			return 3;
+		}
+	}
+
+	@Override
+	public void setExplosionRadius(int radius) {
+		try {
+			creeper.setExplosionRadius(radius);
+		} catch(NoSuchMethodError ex) {
+			// Probably prior to 1.12.2
+		}
 	}
 }
--- src/main/java/com/laytonsmith/abstraction/entities/MCCreeper.java
+++ src/main/java/com/laytonsmith/abstraction/entities/MCCreeper.java
@@ -1,11 +1,11 @@
 
 package com.laytonsmith.abstraction.entities;
 
-/**
- *
- * @author Jason Unger <entityreborn@gmail.com>
- */
 public interface MCCreeper {
 	boolean isPowered();
 	void setPowered(boolean powered);
+	int getMaxFuseTicks();
+	void setMaxFuseTicks(int ticks);
+	int getExplosionRadius();
+	void setExplosionRadius(int radius);
 }
