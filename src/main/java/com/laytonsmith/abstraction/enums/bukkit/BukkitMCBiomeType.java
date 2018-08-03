26b2c61becd7657d4f770ed1b516d2871401983f
PseudoKnight <michael.jt.smith@gmail.com>
2018-08-03 00:59:33 -0700

    BREAKING CHANGE: Update biome types (affects get|set_biome())

--- src/main/java/com/laytonsmith/abstraction/enums/bukkit/BukkitMCBiomeType.java
+++ src/main/java/com/laytonsmith/abstraction/enums/bukkit/BukkitMCBiomeType.java
@@ -1,16 +1,16 @@
 package com.laytonsmith.abstraction.enums.bukkit;
 
 import com.laytonsmith.abstraction.enums.MCBiomeType;
-import com.laytonsmith.abstraction.enums.MCVersion;
 import com.laytonsmith.core.CHLog;
-import com.laytonsmith.core.Static;
 import com.laytonsmith.core.constructs.Target;
 import org.bukkit.block.Biome;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BukkitMCBiomeType extends MCBiomeType<Biome> {
+
+	private static final Map<Biome, MCBiomeType> BUKKIT_MAP = new HashMap<>();
 
 	public BukkitMCBiomeType(MCVanillaBiomeType vanillaBiomeType, Biome biome) {
 		super(vanillaBiomeType, biome);
@@ -30,123 +30,41 @@ public class BukkitMCBiomeType extends MCBiomeType<Biome> {
 		return b.name();
 	}
 
-	public static BukkitMCBiomeType valueOfConcrete(Biome test) {
-		for(MCBiomeType t : mappings.values()) {
-			if(((BukkitMCBiomeType) t).getConcrete().equals(test)) {
-				return (BukkitMCBiomeType) t;
-			}
+	public static MCBiomeType valueOfConcrete(Biome test) {
+		MCBiomeType type = BUKKIT_MAP.get(test);
+		if(type == null) {
+			return NULL;
 		}
-		return (BukkitMCBiomeType) NULL;
-	}
-
-	public static BukkitMCBiomeType valueOfConcrete(String test) {
-		try {
-			return valueOfConcrete(Biome.valueOf(test));
-		} catch (IllegalArgumentException iae) {
-			return (BukkitMCBiomeType) NULL;
-		}
+		return type;
 	}
 
 	// This way we don't take up extra memory on non-bukkit implementations
 	public static void build() {
-		vanilla = new HashMap<>();
-		mappings = new HashMap<>();
 		NULL = new BukkitMCBiomeType(MCVanillaBiomeType.UNKNOWN, null);
-		ArrayList<Biome> counted = new ArrayList<>();
 		for(MCVanillaBiomeType v : MCVanillaBiomeType.values()) {
 			if(v.existsInCurrent()) {
 				Biome type;
 				try {
 					type = getBukkitType(v);
 				} catch (IllegalArgumentException | NoSuchFieldError ex) {
-					CHLog.GetLogger().w(CHLog.Tags.RUNTIME, "Could not find a Bukkit BiomeType for " + v.name(), Target.UNKNOWN);
+					CHLog.GetLogger().w(CHLog.Tags.RUNTIME, "Could not find a Bukkit BiomeType for " + v.name(),
+							Target.UNKNOWN);
 					continue;
 				}
 				BukkitMCBiomeType wrapper = new BukkitMCBiomeType(v, type);
-				vanilla.put(v, wrapper);
-				mappings.put(v.name(), wrapper);
-				counted.add(type);
+				MAP.put(v.name(), wrapper);
+				BUKKIT_MAP.put(type, wrapper);
 			}
 		}
 		for(Biome b : Biome.values()) {
-			if(!counted.contains(b)) {
-				mappings.put(b.name(), new BukkitMCBiomeType(MCVanillaBiomeType.UNKNOWN, b));
+			if(!BUKKIT_MAP.containsKey(b)) {
+				MAP.put(b.name(), new BukkitMCBiomeType(MCVanillaBiomeType.UNKNOWN, b));
+				BUKKIT_MAP.put(b, new BukkitMCBiomeType(MCVanillaBiomeType.UNKNOWN, b));
 			}
 		}
 	}
 
 	private static Biome getBukkitType(MCVanillaBiomeType v) {
-		// A switch statement for values that don't translate directly to a Bukkit value
-		if(Static.getServer().getMinecraftVersion().gte(MCVersion.MC1_9)) {
-			switch(v) {
-				case ICE_PLAINS:
-					return Biome.ICE_FLATS;
-				case MUSHROOM_SHORE:
-					return Biome.MUSHROOM_ISLAND_SHORE;
-				case BEACH:
-					return Biome.BEACHES;
-				case SMALL_MOUNTAINS:
-					return Biome.SMALLER_EXTREME_HILLS;
-				case COLD_TAIGA:
-					return Biome.TAIGA_COLD;
-				case COLD_TAIGA_HILLS:
-					return Biome.TAIGA_COLD_HILLS;
-				case MEGA_TAIGA:
-					return Biome.REDWOOD_TAIGA;
-				case MEGA_TAIGA_HILLS:
-					return Biome.REDWOOD_TAIGA_HILLS;
-				case EXTREME_HILLS_PLUS:
-					return Biome.EXTREME_HILLS_WITH_TREES;
-				case SAVANNA_PLATEAU:
-					return Biome.SAVANNA_ROCK;
-				case JUNGLE_MOUNTAINS:
-					return Biome.MUTATED_JUNGLE;
-				case JUNGLE_EDGE_MOUNTAINS:
-					return Biome.MUTATED_JUNGLE_EDGE;
-				case MEGA_SPRUCE_TAIGA:
-					return Biome.MUTATED_REDWOOD_TAIGA;
-				case MEGA_SPRUCE_TAIGA_HILLS:
-					return Biome.MUTATED_REDWOOD_TAIGA_HILLS;
-				case BIRCH_FOREST_MOUNTAINS:
-					return Biome.MUTATED_BIRCH_FOREST;
-				case BIRCH_FOREST_HILLS_MOUNTAINS:
-					return Biome.MUTATED_BIRCH_FOREST_HILLS;
-				case SAVANNA_MOUNTAINS:
-					return Biome.MUTATED_SAVANNA;
-				case SAVANNA_PLATEAU_MOUNTAINS:
-					return Biome.MUTATED_SAVANNA_ROCK;
-				case MESA_PLATEAU_FOREST:
-					return Biome.MESA_ROCK;
-				case MESA_PLATEAU:
-					return Biome.MESA_CLEAR_ROCK;
-				case SUNFLOWER_PLAINS:
-					return Biome.MUTATED_PLAINS;
-				case DESERT_MOUNTAINS:
-					return Biome.MUTATED_DESERT;
-				case SWAMPLAND_MOUNTAINS:
-					return Biome.MUTATED_SWAMPLAND;
-				case ROOFED_FOREST_MOUNTAINS:
-					return Biome.MUTATED_ROOFED_FOREST;
-				case TAIGA_MOUNTAINS:
-					return Biome.MUTATED_TAIGA;
-				case COLD_TAIGA_MOUNTAINS:
-					return Biome.MUTATED_TAIGA_COLD;
-				case FLOWER_FOREST:
-					return Biome.MUTATED_FOREST;
-				case ICE_PLAINS_SPIKES:
-					return Biome.MUTATED_ICE_FLATS;
-				case EXTREME_HILLS_MOUNTAINS:
-					return Biome.MUTATED_EXTREME_HILLS;
-				case EXTREME_HILLS_PLUS_MOUNTAINS:
-					return Biome.MUTATED_EXTREME_HILLS_WITH_TREES;
-				case MESA_BRYCE:
-					return Biome.MUTATED_MESA;
-				case MESA_PLATEAU_MOUNTAINS:
-					return Biome.MUTATED_MESA_CLEAR_ROCK;
-				case MESA_PLATEAU_FOREST_MOUNTAINS:
-					return Biome.MUTATED_MESA_ROCK;
-			}
-		}
 		return Biome.valueOf(v.name());
 	}
 }
