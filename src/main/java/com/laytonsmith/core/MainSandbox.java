617b699c070dd39db719f237a79a29596b2f721a
LadyCailin <savannahcailin@gmail.com>
2019-03-13 18:59:17 +0100

    Wrote more on topics, write some of the standard library.
    Several more sections have been added to the objects design doc, as well
    as a small section about manifests and annotations. In addition to this,
    a new cmdline tool, new-type has been added. This allows quick and
    correct creation of classes (and types) to be created. Additionally,
    many classes in the standard library have been added, though they are
    not yet compilable, since the compiler does not support this. However,
    they serve as a great base to begin working against for the compiler
    changes, and also to see larger segments of code using the designs
    described in the design doc. There may be some (unintentional) compiler
    errors in the code, but those will be identified later in the process.
    
    Primarily the MethodScript code that is written focuses on supporting
    the manifest file, but several pieces of supporting code have also been
    written, all primarily annotations. The code is subject to be moved
    around and refactored though.
    
    Additionally, a workspace has been created for Visual Studio Code to
    more easily edit the methodscript directory.

--- src/main/java/com/laytonsmith/core/MainSandbox.java
+++ src/main/java/com/laytonsmith/core/MainSandbox.java
@@ -1,36 +1,10 @@
 package com.laytonsmith.core;
 
-import com.laytonsmith.annotations.NonInheritImplements;
 
 /**
  * This class is for testing concepts
  */
 public class MainSandbox {
 
-	public static interface Interface {
-		void method(int i, String b, int j);
-	}
-	@NonInheritImplements(Interface.class)
-	public static class A1 {
-		public void method(int i, String b, int j) {
-			System.out.println("i: " + i + "; b: " + b);
-		}
-	}
-
-	public static class A2 extends A1 {
-		void moreMethods() {
-
-		}
-	}
-
-	public static void main(String[] argv) throws Exception {
-		Interface interf = NonInheritImplements.Helper.Cast(Interface.class, new A1());
-		System.out.println(NonInheritImplements.Helper.Instanceof(new A2(), Interface.class));
-		myMethod(interf);
-	}
-
-	public static void myMethod(Interface iface) {
-		iface.method(12, "string", 123);
-	}
 
 }
