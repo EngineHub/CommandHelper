a61f13c56088ebd8466215f390be8512e21734a0
PseudoKnight <michael.jt.smith@gmail.com>
2019-06-19 07:41:08 -0700

    Comment out broken Mixed argument check for now.
    This code doesn't appear to have worked for over seven years and is currently just slowing down script evaluation time, particularly when isInstanceOf() was added in this workhorse section of the code. It might not even be needed, or should possibly be done at compile time only. In any case, we haven't depended on it for several years.

--- src/main/java/com/laytonsmith/core/Script.java
+++ src/main/java/com/laytonsmith/core/Script.java
@@ -361,17 +361,17 @@ public class Script {
 				Mixed[] ca = new Mixed[a.length];
 				for(int i = 0; i < a.length; i++) {
 					ca[i] = (Mixed) a[i];
-					// TODO: This code is probably outdated and needs to be rethought.
+					// TODO: This code is outdated and needs to be rethought.
 					//CArray, CBoolean, CDouble, CInt, CNull, CString, CVoid, CEntry, CLabel (only to sconcat).
-					if(!(ca[i].isInstanceOf(CArray.class) || ca[i] instanceof CBoolean || ca[i] instanceof CDouble
-							|| ca[i] instanceof CInt || ca[i] instanceof CNull
-							|| ca[i] instanceof CString || ca[i] instanceof CVoid
-							|| ca[i] instanceof IVariable || ca[i] instanceof CEntry || ca[i] instanceof CLabel)
-							&& (!f.getName().equals("__autoconcat__") && (ca[i] instanceof CLabel))) {
-						throw new CRECastException("Invalid Mixed ("
-								+ ca[i].getClass() + ") being passed as an argument to a function ("
-								+ f.getName() + ")", m.getTarget());
-					}
+//					if(!(ca[i].isInstanceOf(CArray.class) || ca[i] instanceof CBoolean || ca[i] instanceof CDouble
+//							|| ca[i] instanceof CInt || ca[i] instanceof CNull
+//							|| ca[i] instanceof CString || ca[i] instanceof CVoid
+//							|| ca[i] instanceof IVariable || ca[i] instanceof CEntry || ca[i] instanceof CLabel)
+//							&& (!f.getName().equals("__autoconcat__") && (ca[i] instanceof CLabel))) {
+//						throw new CRECastException("Invalid Mixed ("
+//								+ ca[i].getClass() + ") being passed as an argument to a function ("
+//								+ f.getName() + ")", m.getTarget());
+//					}
 					while(f.preResolveVariables() && ca[i] instanceof IVariable) {
 						IVariable cur = (IVariable) ca[i];
 						ca[i] = env.getEnv(GlobalEnv.class).GetVarList().get(cur.getVariableName(), cur.getTarget(),
