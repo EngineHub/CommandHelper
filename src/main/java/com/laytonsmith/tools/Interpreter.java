28715f4b5bfcdcd5bb6c0678d22257feeae33733
LadyCailin <savannahcailin@gmail.com>
2019-06-24 23:17:10 +0200

    Checkstyle fix

--- src/main/java/com/laytonsmith/tools/Interpreter.java
+++ src/main/java/com/laytonsmith/tools/Interpreter.java
@@ -1048,7 +1048,7 @@ public final class Interpreter {
 						CommandExecutor.Execute("powershell -command \"& {$md=\\\"[DllImport(`\\\"user32.dll\\\"\\\",SetLastError=true,CharSet=CharSet.Auto)]public static extern IntPtr SendMessageTimeout(IntPtr hWnd,uint Msg,UIntPtr wParam,string lParam,uint fuFlags,uint uTimeout,out UIntPtr lpdwResult);\\\"; $sm=Add-Type -MemberDefinition $md -Name NativeMethods -Namespace Win32 -PassThru;$result=[uintptr]::zero;$sm::SendMessageTimeout(0xffff,0x001A,[uintptr]::Zero,\\\"Environment\\\",2,5000,[ref]$result)}\"");
 					}
 
-				} catch(IOException | IllegalAccessException | InterruptedException | InvocationTargetException ex) {
+				} catch (IOException | IllegalAccessException | InterruptedException | InvocationTargetException ex) {
 					StreamUtils.GetSystemErr().println("Could not install: " + ex + ". You need to run this in Administrator Mode however, did you do that?");
 					System.exit(1);
 				}
