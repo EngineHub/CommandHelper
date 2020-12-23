af004678193e1158a8b40c86ec06831911d48cb9
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2020-12-24 00:14:32 +0100

    Propagate termination through __statements__

--- src/main/java/com/laytonsmith/core/functions/Compiler.java
+++ src/main/java/com/laytonsmith/core/functions/Compiler.java
@@ -581,6 +581,11 @@ public class Compiler {
 		@Override
 		public CClassType getReturnType(Target t, List<CClassType> argTypes,
 				List<Target> argTargets, Environment env, Set<ConfigCompileException> exceptions) {
+			for(CClassType argType : argTypes) {
+				if(argType == null) {
+					return null; // An argument alters control flow, so this function will never return.
+				}
+			}
 			return CVoid.TYPE;
 		}
 
