e8f6e5ec92cbf94d866fad7f657f245ba82bad83
LadyCailin <savannahcailin@gmail.com>
2021-01-28 20:48:07 +0100

    Update WinRegistry for Java 11

--- src/main/java/com/laytonsmith/PureUtilities/Common/WinRegistry.java
+++ src/main/java/com/laytonsmith/PureUtilities/Common/WinRegistry.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.laytonsmith.PureUtilities.Common;
 
 import java.lang.reflect.InvocationTargetException;
@@ -35,8 +54,14 @@ public final class WinRegistry {
 
 	static {
 		try {
-			regOpenKey = USER_CLASS.getDeclaredMethod("WindowsRegOpenKey",
-					new Class[]{int.class, byte[].class, int.class});
+			try {
+				regOpenKey = USER_CLASS.getDeclaredMethod("WindowsRegOpenKey",
+						new Class[]{long.class, byte[].class, int.class});
+			} catch (NoSuchMethodException ex) {
+				// Java < 11
+				regOpenKey = USER_CLASS.getDeclaredMethod("WindowsRegOpenKey",
+						new Class[]{int.class, byte[].class, int.class});
+			}
 			regOpenKey.setAccessible(true);
 			regCloseKey = USER_CLASS.getDeclaredMethod("WindowsRegCloseKey",
 					new Class[]{int.class});
