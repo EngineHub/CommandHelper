23700f2e964fa772fd291df8bf698192880bb29f
Udarczev Anatoliy <toly.udarczev@yandex.ru>
2021-02-21 01:58:55 +0300

    Add block form event

--- src/main/java/com/laytonsmith/core/events/drivers/BlockEvents.java
+++ src/main/java/com/laytonsmith/core/events/drivers/BlockEvents.java
@@ -1519,4 +1519,78 @@ public class BlockEvents {
 			return false;
 		}
 	}
+
+	@api
+	public static class block_form extends AbstractEvent {
+
+		@Override
+		public String getName() {
+			return "block_form";
+		}
+
+		@Override
+		public String docs() {
+			return "{block: <string match> Type of old block state"
+					+ " | newblock: <string match> Type of new block state}"
+					+ "Called when a block is formed or spreads based on world conditions."
+					+ " Cancelling this event block will not be formed."
+					+ "{location: The location the block formed "
+					+ " | block: Type of old block state"
+					+ " | newblock: Type of new block state}"
+					+ "{} "
+					+ "{}";
+		}
+
+		@Override
+		public Driver driver() {
+			return Driver.EXTENSION;
+		}
+
+		@Override
+		public BindableEvent convert(CArray manualObject, Target t) {
+			return null;
+		}
+
+		@Override
+		public Version since() {
+			return MSVersion.V3_3_4;
+		}
+
+		@Override
+		public boolean matches(Map<String, Mixed> prefilter, BindableEvent e) throws PrefilterNonMatchException {
+			if(e instanceof MCBlockFadeEvent) {
+				MCBlockFadeEvent event = (MCBlockFadeEvent) e;
+
+				String newBlock = event.getNewState().getType().getName();
+				String block = event.getBlock().getType().getName();
+
+				Prefilters.match(prefilter, "newblock", newBlock, Prefilters.PrefilterType.STRING_MATCH);
+				Prefilters.match(prefilter, "block", block, Prefilters.PrefilterType.STRING_MATCH);
+
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public Map<String, Mixed> evaluate(BindableEvent e) throws EventException {
+			if(!(e instanceof MCBlockFadeEvent)) {
+				throw new EventException("Cannot convert event to MCBlockFadeEvent");
+			} else {
+				MCBlockFadeEvent event = (MCBlockFadeEvent) e;
+				Target t = Target.UNKNOWN;
+				Map<String, Mixed> mapEvent = this.evaluate_helper(e);
+				mapEvent.put("block", new CString(event.getBlock().getType().getName(), t));
+				mapEvent.put("newblock", new CString(event.getNewState().getType().getName(), t));
+				mapEvent.put("location", ObjectGenerator.GetGenerator().location(event.getBlock().getLocation(), false));
+				return mapEvent;
+			}
+		}
+
+		@Override
+		public boolean modifyEvent(String key, Mixed value, BindableEvent e) {
+			return false;
+		}
+	}
 }
