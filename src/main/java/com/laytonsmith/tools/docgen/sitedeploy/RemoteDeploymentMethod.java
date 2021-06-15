7a4541b649008b46f36b54d6d8bcf5912556f27c
LadyCailin <savannahcailin@gmail.com>
2021-06-16 01:13:13 +0200

    Use correct slash in site deploy

--- src/main/java/com/laytonsmith/tools/docgen/sitedeploy/RemoteDeploymentMethod.java
+++ src/main/java/com/laytonsmith/tools/docgen/sitedeploy/RemoteDeploymentMethod.java
@@ -18,6 +18,13 @@ class RemoteDeploymentMethod implements DeploymentMethod {
 
 	@Override
 	public boolean deploy(InputStream data, String toLocation, String overrideIdRsa) throws IOException {
+		// TODO We actually are assuming the system we're connecting to is linux, however, that may not
+		// be true. We should have two RemoteDeploymentMethods for each OS so that we can support
+		// either, and add an option to be able to override that if the remote system is not Linux.
+		// There's no real way for us to guess the OS though, so we can't do that automatically. But
+		// linux is a super good guess if we're using ssh. However, swapping to an rsync method may
+		// make this moot anyways.
+		toLocation = toLocation.replace("\\", "/");
 		return SSHWrapper.SCPWrite(data, remote + toLocation, overrideIdRsa);
 	}
 
