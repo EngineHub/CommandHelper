96ff7b102c2946ad590f1fd5831aabadc6db02e3
LadyCailin <savannahcailin@gmail.com>
2021-08-07 00:19:58 +0200

    Add commit info to downloads page

--- src/main/resources/siteDeploy/resources/js/download.js
+++ src/main/resources/siteDeploy/resources/js/download.js
@@ -1,6 +1,13 @@
 
 // This script is only intended for use on the downloads page.
 (function() {
+
+	function escapeHTML(unsafeText) {
+		let div = document.createElement('div');
+		div.innerText = unsafeText;
+		return div.innerHTML;
+	}
+
 	function formatList(archive, id, showRecommended=true) {
 		$.getJSON("https://apps.methodscript.com/builds/" + archive, function(data) {
 			console.log("Got " + data.length + " results for " + archive);
@@ -14,7 +21,35 @@
 			var html = "<div><ul>";
 			var first = true;
 			for (const version of data) {
-				html += "<li><a href='https://apps.methodscript.com" + version.link + "'>" + version.buildId + "</a>";
+				html += "<li><a href='https://apps.methodscript.com" + version.link + "'>";
+				html += version.buildId + "</a> ";
+				if(archive == "commandhelperjar") {
+					// This is the only one that has meta information at this stage
+					if(version.sha) {
+						html += "[";
+						if(version.commitDetails) {
+							html += "<span id=\"commit_sha_" + version.sha + "\">";
+						}
+						html += "<a href='https://github.com/EngineHub/CommandHelper/commit/" + version.sha + "'>"
+							+ version.sha.substring(0, 7) + "</a>"
+						if(version.commitDetails) {
+							html += "</span>";
+						}
+						html += "] ";
+
+						if(version.commitDetails) {
+							pageRender.then(function() {
+								setTimeout(function() {
+									$("#commit_sha_" + version.sha).tooltipster({
+										content: $("<code><pre>"
+											+ escapeHTML(version.commitDetails)
+											+ "</pre></code>")
+									});
+								}, 1);
+							});
+						}
+					}
+				}
 				if(showRecommended && first) {
 					html += " (Recommended)";
 					first = false;
@@ -30,4 +65,30 @@
 	formatList("commandhelperjar", "commandhelperjar");
 	formatList("commandhelperpinned", "commandhelperpinned", false);
 	formatList("commandhelperwindowsinstaller", "commandhelperwindowsinstaller");
+
+	$(".commit_tooltip").tooltipster({
+		contentCloning: true,
+		interactive: false,
+		theme: 'tooltipster-shadow',
+		maxWidth: 400,
+		delay: 600,
+		functionBefore: function (instance, helper) {
+			var f = helper.origin.innerText;
+			console.debug("Triggering tooltip for " + f);
+			api.done(function (resp) {
+				var fd = resp.functions[f];
+				if (fd !== null) {
+					var $tt = $(instance.content());
+					$tt.find(".ret").html(fd.ret);
+					$tt.find(".name").html(fd.name);
+					$tt.find(".args").html(fd.args);
+					var p = doStandardReplacement(fd.desc);
+					p.then(function (rep) {
+						$tt.find(".desc").html(rep);
+						instance.reposition();
+					});
+				}
+			});
+		}
+	});
 })();
