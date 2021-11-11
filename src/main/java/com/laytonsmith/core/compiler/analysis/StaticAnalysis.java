4f76582dee5f51b3e6a0e304e042d7f49cf13839
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2021-11-11 19:04:55 +0100

    Move instanceof logic to InstanceofUtil + Fix SA instanceof reference
    - Move instanceof logic from SA to InstanceofUtil. The new generics check here is questionable as the actual generic geclaration from instanceofThis is never used, and some other LeftHandGenericUse is passed in instead. This could use a revision that is out of scope for this commit.
    - Fix SA instanceof `(CClassType, CClassType)` method signature being redirected to the `(Mixed, CClassType)` instanceof method, effectively making all types `ClassType` instead of their actual type.

--- src/main/java/com/laytonsmith/core/compiler/analysis/StaticAnalysis.java
+++ src/main/java/com/laytonsmith/core/compiler/analysis/StaticAnalysis.java
@@ -7,9 +7,7 @@ import com.laytonsmith.core.constructs.CClassType;
 import com.laytonsmith.core.constructs.CFunction;
 import com.laytonsmith.core.constructs.CKeyword;
 import com.laytonsmith.core.constructs.CLabel;
-import com.laytonsmith.core.constructs.CNull;
 import com.laytonsmith.core.constructs.CString;
-import com.laytonsmith.core.constructs.CVoid;
 import com.laytonsmith.core.constructs.IVariable;
 import com.laytonsmith.core.constructs.InstanceofUtil;
 import com.laytonsmith.core.constructs.Target;
@@ -430,18 +428,8 @@ public class StaticAnalysis {
 	public static void requireType(CClassType type, CClassType expected,
 			Target t, Environment env, Set<ConfigCompileException> exceptions) {
 
-		// Handle types that cause exceptions in the InstanceofUtil isInstanceof check.
-		if(type.equals(expected) || type == CClassType.AUTO || type == CNull.TYPE) {
-			return;
-		}
-		if(type == CVoid.TYPE || expected == CVoid.TYPE || expected == CNull.TYPE) {
-			exceptions.add(new ConfigCompileException("Expected type " + expected.getSimpleName()
-					+ ", but received type " + type.getSimpleName() + " instead.", t));
-			return;
-		}
-
-		// Handle 'normal' types.
-		if(!InstanceofUtil.isInstanceof(type, expected, env)) {
+		// Generate an exception if the given type is not instanceof the expected type.
+		if(!InstanceofUtil.isInstanceof(type, expected, null, env)) {
 			exceptions.add(new ConfigCompileException("Expected type " + expected.getSimpleName()
 				+ ", but received type " + type.getSimpleName() + " instead.", t));
 		}
@@ -461,9 +449,7 @@ public class StaticAnalysis {
 
 		// Return if the type is instanceof any expected type.
 		for(CClassType exp : expected) {
-			if(type.equals(exp) || type == CClassType.AUTO || type == CNull.TYPE
-					|| (type != CVoid.TYPE && exp != CVoid.TYPE && exp != CNull.TYPE)
-					|| InstanceofUtil.isInstanceof(type, exp, env)) {
+			if(InstanceofUtil.isInstanceof(type, exp, null, env)) {
 				return;
 			}
 		}
