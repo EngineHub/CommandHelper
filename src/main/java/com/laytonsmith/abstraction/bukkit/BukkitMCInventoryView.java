15d2b5b8678c9fd6710df48926a6c7ee949268c0
PseudoKnight <michael.jt.smith@gmail.com>
2024-06-15 17:25:29 -0700

    Fix pinventory_holder() in versions prior to 1.21
    InventoryView was changed from an abstract class to an interface in the latest spigot commit

--- src/main/java/com/laytonsmith/abstraction/bukkit/BukkitMCInventoryView.java
+++ src/main/java/com/laytonsmith/abstraction/bukkit/BukkitMCInventoryView.java
@@ -1,11 +1,13 @@
 package com.laytonsmith.abstraction.bukkit;
 
+import com.laytonsmith.PureUtilities.Common.ReflectionUtils;
 import com.laytonsmith.abstraction.MCHumanEntity;
 import com.laytonsmith.abstraction.MCInventory;
 import com.laytonsmith.abstraction.MCInventoryView;
 import com.laytonsmith.abstraction.MCItemStack;
 import com.laytonsmith.abstraction.bukkit.entities.BukkitMCHumanEntity;
 import com.laytonsmith.abstraction.enums.MCInventoryType;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.InventoryView;
 
 public class BukkitMCInventoryView implements MCInventoryView {
@@ -33,27 +35,27 @@ public class BukkitMCInventoryView implements MCInventoryView {
 
 	@Override
 	public MCInventory getBottomInventory() {
-		return new BukkitMCInventory(iv.getBottomInventory());
+		return new BukkitMCInventory(ReflectionUtils.invokeMethod(iv, "getBottomInventory"));
 	}
 
 	@Override
 	public MCInventory getTopInventory() {
-		return new BukkitMCInventory(iv.getTopInventory());
+		return new BukkitMCInventory(ReflectionUtils.invokeMethod(iv, "getTopInventory"));
 	}
 
 	@Override
 	public void close() {
-		iv.close();
+		ReflectionUtils.invokeMethod(iv, "close");
 	}
 
 	@Override
 	public int countSlots() {
-		return iv.countSlots();
+		return ReflectionUtils.invokeMethod(iv, "countSlots");
 	}
 
 	@Override
 	public int convertSlot(int rawSlot) {
-		return iv.convertSlot(rawSlot);
+		return ReflectionUtils.invokeMethod(iv, "convertSlot", rawSlot);
 	}
 
 	@Override
@@ -63,26 +65,26 @@ public class BukkitMCInventoryView implements MCInventoryView {
 
 	@Override
 	public MCHumanEntity getPlayer() {
-		return new BukkitMCHumanEntity(iv.getPlayer());
+		return new BukkitMCHumanEntity(ReflectionUtils.invokeMethod(iv, "getPlayer"));
 	}
 
 	@Override
 	public String getTitle() {
-		return iv.getTitle();
+		return ReflectionUtils.invokeMethod(iv, "getTitle");
 	}
 
 	@Override
 	public MCInventoryType getType() {
-		return MCInventoryType.valueOf(this.iv.getType().name());
+		return MCInventoryType.valueOf(((InventoryType) ReflectionUtils.invokeMethod(iv, "getType")).name());
 	}
 
 	@Override
 	public void setCursor(MCItemStack item) {
-		iv.setCursor(((BukkitMCItemStack) item).__ItemStack());
+		ReflectionUtils.invokeMethod(iv, "setCursor", ((BukkitMCItemStack) item).__ItemStack());
 	}
 
 	@Override
 	public void setItem(int slot, MCItemStack item) {
-		iv.setItem(slot, (((BukkitMCItemStack) item).__ItemStack()));
+		ReflectionUtils.invokeMethod(iv, "setItem", slot, (((BukkitMCItemStack) item).__ItemStack()));
 	}
 }
