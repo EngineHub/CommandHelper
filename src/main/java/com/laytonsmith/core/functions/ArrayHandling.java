EngineHub/CommandHelper

commit 520a2bb1b2e771f97f7c79ca06fb59b99389b8fe

Widen SA type as well

src/main/java/com/laytonsmith/core/functions/ArrayHandling.java
@@ -296,7 +296,7 @@ public class ArrayHandling {
 		public CClassType getReturnType(Target t, List<CClassType> argTypes, List<Target> argTargets,
 				Environment env, Set<ConfigCompileException> exceptions) {
 			if(argTypes.size() == 2 || argTypes.size() == 3) {
-				StaticAnalysis.requireType(argTypes.get(0), ArrayAccess.TYPE, argTargets.get(0), env, exceptions);
+				StaticAnalysis.requireType(argTypes.get(0), com.laytonsmith.core.natives.interfaces.Iterable.TYPE, argTargets.get(0), env, exceptions);
 				StaticAnalysis.requireAnyType(argTypes.get(1),
 						new CClassType[] {CInt.TYPE, CSlice.TYPE, CString.TYPE}, argTargets.get(1), env, exceptions);
 				if(argTypes.size() == 3) {

src/main/java/com/laytonsmith/core/natives/interfaces/ArrayAccess.java
@@ -87,7 +87,7 @@ public interface ArrayAccess extends Booleanish {
 	 * int start = Math.min(begin, end);
 	 * int stop = Math.max(begin, end);
 	 * int step = (begin &lt;= end) ? 1 : -1;
-	 * for (int i = start; i != stop; i += step) { ... }
+	 * for(int i = start; i != stop; i += step) { ... }
 	 * </code></pre>
 	 *
 	 * @param begin

commit e61eaa662f72598a914c853df1329563aaaabd92

Fix array_get typecheck
Issue introduced in 520a2bb1b.
Fixes compile error when using `array_get()` on `Exception` type objects.

src/main/java/com/laytonsmith/core/functions/ArrayHandling.java
@@ -297,7 +297,8 @@ public class ArrayHandling {
 		public CClassType getReturnType(Target t, List<CClassType> argTypes, List<Target> argTargets,
 				Environment env, Set<ConfigCompileException> exceptions) {
 			if(argTypes.size() == 2 || argTypes.size() == 3) {
-				StaticAnalysis.requireType(argTypes.get(0), com.laytonsmith.core.natives.interfaces.Iterable.TYPE, argTargets.get(0), env, exceptions);
+				StaticAnalysis.requireAnyType(argTypes.get(0), new CClassType[] {ArrayAccess.TYPE,
+						com.laytonsmith.core.natives.interfaces.Iterable.TYPE}, argTargets.get(0), env, exceptions);
 				StaticAnalysis.requireAnyType(argTypes.get(1),
 						new CClassType[]{CInt.TYPE, CSlice.TYPE, CString.TYPE}, argTargets.get(1), env, exceptions);
 				if(argTypes.size() == 3) {